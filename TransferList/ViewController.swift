a5a8c45915b8227b1ba3973bee917717854a381b
Yaser <YaserBahrami@outlook.com>
2025-09-19 20:47:00 +0300

    Initial Commit

---
+++ TransferList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TransferList
+//
+//  Created by Yaser Bahrami on 19.09.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TransferListTests/TransferListTests.swift
@@ -0,0 +1,17 @@
+//
+//  TransferListTests.swift
+//  TransferListTests
+//
+//  Created by Yaser Bahrami on 19.09.2025.
+//
+
+import Testing
+@testable import TransferList
+
+struct TransferListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
